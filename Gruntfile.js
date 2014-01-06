290571593a2d989b1dec502580d609c75b7dcec5
Arian Stolwijk <stolwijk.arian@gmail.com>
2014-01-06 12:50:28 +0100

    Update wrapup to 1.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,4 +9,22 @@
 "use strict";
 
 module.exports = function(grunt) {
+
+  grunt.loadTasks('tasks');
+
+  grunt.initConfig({
+    wrapup: {
+      build: {
+        requires: {
+          "./test/main.js": true,
+          "prime": "prime"
+        },
+        options: {
+          "compress": true,
+          "output": "built.js"
+        }
+      }
+    }
+  });
+
 };
--- tasks/wrapup.js
+++ tasks/wrapup.js
@@ -16,18 +16,12 @@ module.exports = function(grunt) {
     grunt.registerMultiTask('wrapup', 'Wraps node modules into web modules', function() {
 
         var done = this.async()
-
-        var wrup = wrapup()
-
-        if (this.data.options){
-            wrup.options(this.data.options)
-        }
+        var wrup = wrapup(this.data.options);
 
         if (this.data.requires){
             for (var name in this.data.requires){
                 var r = this.data.requires[name]
                 var file = typeof r == 'string' ? r : name
-                file = path.join(process.cwd(), file)
                 if (typeof r == 'string') wrup.require(name, file)
                 else wrup.require(file)
             }
